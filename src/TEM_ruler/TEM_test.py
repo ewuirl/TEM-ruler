c1721c5aeeb4d98146b833b4055b4d72f0b9788d
Emily <emwu@mit.edu>
2022-04-19 16:24:52 -0400

    testing commandline stuff

--- src/TEM_ruler/TEM_test.py
+++ src/TEM_ruler/TEM_test.py
@@ -1,6 +1,9 @@
 import argparse
 
-if __name__ == "__main__":
+def simple_test_func():
+    print("potato")
+
+def main():
     # Argument parser
     parser = argparse.ArgumentParser(description="Test running from the command line")
     parser.add_argument("print_string", type=str, help="thing to print")
@@ -11,3 +14,16 @@ if __name__ == "__main__":
     print("saving a file")
     with open(f"{print_string}.txt", "w") as file:
         file.write(print_string)
+
+if __name__ == "__main__":
+    main()
+#     # Argument parser
+#     parser = argparse.ArgumentParser(description="Test running from the command line")
+#     parser.add_argument("print_string", type=str, help="thing to print")
+    
+#     args = parser.parse_args()
+#     print_string = args.print_string
+#     print(print_string)
+#     print("saving a file")
+#     with open(f"{print_string}.txt", "w") as file:
+#         file.write(print_string)
